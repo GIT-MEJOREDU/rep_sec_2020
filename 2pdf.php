76e6f81965cfc3b4bb9eb48327178180e8466a88
Isidro López García <isidro.lopez@mejoredu.gob.mx>
2020-09-02 19:07:32 -0500

    se modifico ruta para generar archivo html ../rep_sec_2020/..

--- 2pdf.php
+++ 2pdf.php
@@ -8,14 +8,14 @@ if(is_array($arrv)){
 	$cct_turno = $arrv['vent'];
 	$tipo = $arrv['tipo'];
 	$randy = rand();
-	shell_exec("google-chrome --headless --dump-dom http://localhost:9090/reportes_ems/test.php?va=$cct_turno > ./salida/$randy$cct_turno.php");
+	shell_exec("google-chrome --headless --dump-dom http://localhost:9090/rep_sec_2020/test.php?va=$cct_turno > ./salida/$randy$cct_turno.php");
 	$html= file_get_contents("./salida/$randy$cct_turno.php");
 	$mpdf = new mPDF ('c', 'Letter');
 	$mpdf->SetHTMLFooter ('
 		<div style="font-size: 13px; font-weight: bold; text-align: right; color: #333e75; vertical-align: bottom;padding-right: 50px;">
 			<span>Página - {PAGENO} de {nbpg}</span><br/>
 			<span >
-				Para más información sobre los resultados de tu escuela visita <a href="https://www.inee.edu.mx/index.php/sire-inee">www.inee.edu.mx/index.php/sire-inee</a>
+				Para más información sobre los resultados de tu escuela visita <a href="https://www.gob.mx/mejoredu">https://www.gob.mx/mejoredu</a>
 			</span>
 		</div>'
 	);
